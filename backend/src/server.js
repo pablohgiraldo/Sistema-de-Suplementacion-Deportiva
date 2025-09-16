3bf30172d3ea4f623acc04566aea2ea94bebbdba
Pablo Hurtado Giraldo <pablo.giral04@gmail.com>
2025-09-16 11:12:38 -0500

    fix: Agregar puerto 5174 a configuración CORS para frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -43,6 +43,7 @@ app.use(
         "http://localhost:4173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
         "http://127.0.0.1:3000"
       ];
       
